fabad4e7e208e56aeeded38cb06244fc6fe81895
Priyansh Khare <priyanshkhare2005@gmail.com>
2025-06-23 09:46:30 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treats all code as newer version of js
+
+//alert(3+3) // we are using node.js not browser
+
+// code readability should be high
+
+let name = "Priyansh" //string
+let age = 18 
+let isLoggedIn = false //boolean
+let state = null
+
+// number => 2 to power 53
+// bigint for very large number especially in trading
+//string => ""
+//boolean => true/false
+// null => standalone value
+// undefined => not assigned yet
+// symbol => for finding uniqueness
+
+
+// object
+
+console.log(typeof "Priyansh"); // string
+
+console.log(typeof null); // object
