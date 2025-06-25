ff14f88a2b2534d39b60028e1ea07d9a10011d60
Priyansh Khare <priyanshkhare2005@gmail.com>
2025-06-25 16:26:44 +0000

    Conversion of Datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,32 @@
+// let score = "33abc"
+
+// console.log (typeof score)
+// console.log (typeof (score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber) //NaN
+
+
+// let score = null
+// console.log(typeof score) //object
+// console.log(score) //null
+
+// for a string NaN
+// number easily converted
+// true => 1
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof (booleanIsLoggedIn)) //boolean
+console.log(booleanIsLoggedIn) //true
+
+// "" => false
+// "abc" => true
+
+
+let someNumber =33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
