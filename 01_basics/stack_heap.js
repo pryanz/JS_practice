5ca815ef9c21da33decb570514f2434e452a5f10
Priyansh Khare <priyanshkhare2005@gmail.com>
2025-06-26 03:41:14 +0000

    stack and heap memory

---
+++ 01_basics/stack_heap.js
@@ -0,0 +1,21 @@
+let myName = "Priyansh"
+let anotherName = myName
+anotherName = "Khare"
+
+console.log(myName);
+console.log(anotherName);
+
+
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+
+let userTwo = userOne
+
+userTwo.email = "priyansh@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
